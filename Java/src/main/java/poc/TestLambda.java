7751a8ddfc33d24aeb687abcd90249b0cb345a03
PhilLarkin <philiplarkin8383@gmail.com>
2021-08-13 00:05:26 +0100

    tweaks fixes and further experimentation

--- Java/src/main/java/poc/TestLambda.java
+++ Java/src/main/java/poc/TestLambda.java
@@ -3,10 +3,23 @@ package poc;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class TestLambda implements RequestHandler<Object, OutputObject>{
+
+    static DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
     static int staticVarCount = 0;
     int instanceVarCount = 0;
+    String dateTime;
+
+
+    public TestLambda(){
+        super();
+        dateTime = formatter.format(new Date());
+    }
 
     public OutputObject handleRequest(final Object event, final Context context) {
 
@@ -15,7 +28,8 @@ public class TestLambda implements RequestHandler<Object, OutputObject>{
 
         final String rsBody = "Java Lambda:\n"
                             + "I've seen that static variable " + staticVarCount + " time(s).\n"
-                            + "I've seen that instance variable " + instanceVarCount + " times(s).";
+                            + "I've seen that instance variable " + instanceVarCount + " times(s).\n"
+                            + "First invoked = " + dateTime;
 
         System.out.println(rsBody);
         return new OutputObject(200, rsBody);
--- Python/tearDown.sh
+++ Python/tearDown.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm testLambda.zip
-aws lambda delete-function --function-name pyhtonTest
+aws lambda delete-function --function-name pythonLocalStorage
